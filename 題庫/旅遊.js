1114bf8be0bdfef9ccc049d706ad63d149bde9b1
CammyLemon <cammy951003@gmail.com>
2025-05-04 03:14:46 +0800

    Create 旅遊.js

---
+++ 題庫/旅遊.js
@@ -0,0 +1,282 @@
+vocabulary["旅遊"] =  [
+{
+word: "itinerary",
+Chinese: "行程",
+definition: "(n.) a detailed plan or route of a journey",
+sentence: "The tour operator will arrange transport and plan your itinerary.",
+options: ["immigration", "itinerary", "transit", "halfway"]
+},
+{
+word: "accommodation",
+Chinese: "住處",
+definition: "(n.) a room, group of rooms, or building in which someone may live or stay",
+sentence: "You should book your accommodation early if you're traveling on a budget.",
+options: ["accommodation", "reimbursement", "concierge", "itinerary"]
+},
+{
+word: "amenities",
+Chinese: "娛樂設施",
+definition: "(n.) something that is intended to make life more pleasant or comfortable for the people in a town, hotel, or other place",
+sentence: "The council has some spare cash, which it proposes to spend on public amenities.",
+options: ["itinerant", "itinerary", "excursion", "amenities"]
+},
+{
+word: "excursion",
+Chinese: "短程旅行；遠足",
+definition: "(n.) a short journey usually made for pleasure, often by a group of people",
+sentence: "We went on a day excursion to the nearby island.",
+options: ["transit", "compartment", "embark", "excursion"]
+},
+{
+word: "itinerant",
+Chinese: "巡迴的；流動的",
+definition: "(a.) travelling from one place to another, usually to work for a short period",
+sentence: "The itinerant guide spoke several languages fluently.",
+options: ["itinerant", "detour", "incidental", "timetable"]
+},
+{
+word: "reimbursement",
+Chinese: "報銷",
+definition: "(n.) the act of paying back money to someone who has spent it for you or lost it because of you",
+sentence: "Employees can apply for reimbursement of travel expenses.",
+options: ["reimbursement", "resort", "amenities", "lodging"]
+},
+{
+word: "reimburse",
+Chinese: "補償",
+definition: "(v.) to pay back money to someone who has spent it for you or lost it because of you ",
+sentence: "The airline will reimburse me for the amount they had overcharged me.",
+options: ["reimburse", "fasten", "transit", "cruise"]
+},
+{
+word: "transit",
+Chinese: "轉機；運輸",
+definition: "(n.) the movement of goods or people from one place to another",
+sentence: "Passengers can wait in the transit lounge between flights. ",
+options: ["transit", "concierge", "detour", "freight"]
+},
+{
+word: "concierge",
+Chinese: "櫃台人員",
+definition: "(n.) someone who is employed in a hotel, private club, or similar place to help guests with anything they need",
+sentence: "You can ask the concierge to call a taxi for you. ",
+options: ["concierge", "detour", "itinerant", "escort"]
+},
+{
+word: "resort",
+Chinese: "渡假勝地",
+definition: "(n.) a place where many people go for rest, sport, or another stated purpose ",
+sentence: "There are many summer resorts in the mountains.",
+options: ["resort", "porter", "brochure", "itinerant"]
+},
+{
+word: "jet lag",
+Chinese: "時差造成的疲累",
+definition: "(n.) the feeling of tiredness and confusion that people experience after travelling by plane to a place where the time is different",
+sentence: "I couldn’t sleep well due to jet lag after the long flight.",
+options: ["jet lag", "layover", "claim tag", "excursion"]
+},
+{
+word: "embark",
+Chinese: "登機；登船",
+definition: "(v.) to go onto a ship, plane or car",
+sentence: "We will embark on our cruise at 5 p.m.",
+options: ["embark", "reimburse", "refund", "deck"]
+},
+{
+word: "immigration",
+Chinese: "移民；入境",
+definition: "(n.) the act of someone coming to live in a different country",
+sentence: "There are strict limits on immigration into the country.",
+options: ["immigration", "refund", "deck", "voyage"]
+},
+{
+word: "currency exchange",
+Chinese: "貨幣交換所",
+definition: "(n.) an office where you can change one money into another",
+sentence: "There are currency exchanges and ATMs in most big towns and cities.",
+options: ["currency exchange", "turbulence", "deluxe", "jet lag"]
+},
+{
+word: "red-eye flight",
+Chinese: "紅眼航班",
+definition: "(n.) a plane journey during the night, usually one that is over a long distance",
+sentence: "I took a red-eye flight to save on hotel costs.",
+options: ["terminal", "red-eye flight", "duty-free shop", "non-stop flight"]
+},
+{
+word: "detour",
+Chinese: "繞路",
+definition: "(n.) a different route, which may not be the shortest way, but can avoid something such as a traffic jam",
+sentence: "You'd be wise to make a detour to avoid the roadworks.",
+options: ["detour", "freight", "resort", "bypass"]
+},
+{
+word: "sightseeing",
+Chinese: "觀光",
+definition: "(n.) the activity of visiting interesting places, especially by people on holiday",
+sentence: "We had plenty of time to go sightseeing in Paris after the conference.",
+options: ["sightseeing", "transit", "fasten", "brochure"]
+},
+{
+word: "cruise",
+Chinese: "郵輪旅遊",
+definition: "(n.) a vacation spent on a ship that sails the ocean, usually one that is over a long distance",
+sentence: "He and his wife went on a cruise to the Bahamas.",
+options: ["cruise", "timetable", "compartment", "concierge"]
+},
+{
+word: "cruise",
+Chinese: "巡航",
+definition: "(v.) sail about in an area without a precise destination",
+sentence: "The coastguard cutter cruised along the coast looking for smugglers.",
+options: ["cruise", "escort", "resort", "fasten"]
+},
+{
+word: "incidental",
+Chinese: "附帶的",
+definition: "(a.) less important than the thing something is connected with or part of ",
+sentence: "When you check out of the hotel, they might charge you incidental fees in addition to the main bill.",
+options: ["incidental", "turbulence", "lodging", "deluxe"]
+},
+{
+word: "lodging",
+Chinese: "住宿",
+definition: "(n.) a temporary place to stay",
+sentence: "The price includes board and lodging.",
+options: ["lodging", "voyage", "escort", "freight"]
+},
+{
+word: "group rate",
+Chinese: "團體價",
+definition: "(n.) an economical price for a group of people",
+sentence: "The group rate is cost-effective only if we have eight or more people.",
+options: ["group rate", "time rate", "sightseeing", "hotel lobby"]
+},
+{
+word: "halfway",
+Chinese: "中途的",
+definition: "(a.) in the middle of something, or at a place that is equally far from two other places",
+sentence: "Tuvalu is made of nine small islands about halfway between Australia and Hawaii.",
+options: ["halfway", "voyage", "bag", "compartment"]
+},
+{
+word: "escort",
+Chinese: "護送",
+definition: "(n.) to go with a person or vehicle to make certain that he, she, or it leaves or arrives safely",
+sentence: "Jim got so drunk that the bouncers had to escort him out of the club.",
+options: ["escort", "resort", "deck", "accommodate"]
+},
+{
+word: "deck",
+Chinese: "甲板",
+definition: "(n.) a flat area for walking on, built across the space between the sides of a boat",
+sentence: "We spent the afternoon sitting on deck enjoying the view.",
+options: ["deck", "vacant", "escort", "timetable"]
+},
+{
+word: "bypass",
+Chinese: "越過",
+definition: "(v.) to ignore a rule or official authority",
+sentence: "I was able to bypass the airport because I was traveling with a VIP.",
+options: ["bypass", "voyage", "halfway", "escort"]
+},
+{
+word: "porter",
+Chinese: "搬運工",
+definition: "(n.) a person whose job is to carry things, especially travelers' bags at railway stations, airports, hotels etc.",
+sentence: "There aren't any porters, so we'll have to find a trolley for the luggage.",
+options: ["porter", "detour", "vacant", "bypass"]
+},
+{
+word: "turbulence",
+Chinese: "亂流",
+definition: "(n.) strong sudden movements within air or water",
+sentence: "The severe turbulence caused injuries to 5 flight attendants and 3 passengers.",
+options: ["turbulence", "expedition", "currency exchange", "halfway"]
+},
+{
+word: "fasten",
+Chinese: "繫上",
+definition: "(v.) to cause something to become firmly fixed together, or in position, or closed",
+sentence: "Passengers were told to fasten their seat belts before the plane made an emergency landing.",
+options: ["fasten", "compartment", "embark", "resort"]
+},
+{
+word: "compartment",
+Chinese: "包廂",
+definition: "(n.) one of the separate areas inside a vehicle, especially a train",
+sentence: "There were ten compartments in each car of the train.",
+options: ["compartment", "cruise", "expedition", "immigration"]
+},
+{
+word: "timetable",
+Chinese: "行程表",
+definition: "(n.) a detailed plan showing when events or activities will happen",
+sentence: "The heavy fog upset our timetable for the trip.",
+options: ["timetable", "reimbursement", "lodging", "excursion"]
+},
+{
+word: "refund",
+Chinese: "退款",
+definition: "(v.) to give someone their money back, especially because they are not satisfied with a product or service",
+sentence: "The customer asked the store to refund her money for the broken product.",
+options: ["refund", "reimbursement", "resort", "lodging"]
+},
+{
+word: "refund",
+Chinese: "退款",
+definition: "(n.) an amount of money that is given back to you, especially because you are not happy with a product or service",
+sentence: "I intend to return an item that you bought yesterday and ask for a refund.",
+options: ["refund", "excursion", "amenity", "bypass"]
+},
+{
+word: "brochure",
+Chinese: "小手冊",
+definition: "(n.) a type of small magazine that contains pictures and information on a product",
+sentence: "The picture of French countryside in the travel brochure tempted me to go on an excursion.",
+options: ["brochure", "sightseeing", "bypass", "porter"]
+},
+{
+word: "freight",
+Chinese: "貨物",
+definition: "(n.) goods that are carried from one place to another, by ship, aircraft, train, or truck, or the system of transporting these goods",
+sentence: "The ship carries both freight and passengers.",
+options: ["freight", "turbulence", "brochure", "transit"]
+},
+{
+word: "all-inclusive",
+Chinese: "全部包括的",
+definition: "(a.) including everything, especially including the cost of all services that are offered",
+sentence: "The travel agency may have an all-inclusive package.",
+options: ["all-inclusive", "vacant", "jet lag", "deluxe"]
+},
+{
+word: "deluxe",
+Chinese: "奢華的",
+definition: "(a.) very comfortable and of very high quality",
+sentence: "The deluxe suite also comes with a very nice bar.",
+options: ["deluxe", "incidental", "compartment", "itinerary"]
+},
+{
+word: "vacant",
+Chinese: "未被佔用的,空缺的",
+definition: "(a.) not filled or occupied; available to be used",
+sentence: "A vacant apartment in New York City is very difficult to find.",
+options: ["vacant", "deluxe", "itinerant", "escort"]
+},
+{
+word: "expedition",
+Chinese: "探索；遠征",
+definition: "(n.) an organized journey for a particular purpose",
+sentence: "Explorer Ernest Shackleton led his first expedition to Antarctica in 1908.",
+options: ["expedition", "amenities", "freight", "timetable"]
+},
+{
+word: "voyage",
+Chinese: "航程；航行",
+definition: "(n.) an act or instance of traveling",
+sentence: "The elderly couple went on a voyage around the world.",
+options: ["voyage", "brochure", "detour", "turbulence"]
+}
+]
