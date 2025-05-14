2562557c3df973cd4fbc16561e4e3b55476d399d
CammyLemon <cammy951003@gmail.com>
2025-05-14 15:46:01 +0800

    Create cooking.js

---
+++ 題庫/cooking.js
@@ -0,0 +1,150 @@
+[
+    {
+word: "stew",
+Chinese: "燉煮",
+definition: "(v.) cook slowly in a small amount of liquid",
+sentence: "We stewed the tomatoes with peppers. ",
+options: ["stew", "scrub", "rinse", "fiber"]
+},
+{
+word: "scrub",
+Chinese: "刷洗",
+definition: "(v.) to rub something hard in order to clean it",
+sentence: "Did you scrub your hands with soap?",
+options: ["scrub", "detour", "spoil", "sift"]
+},
+{
+word: "hectic",
+Chinese: "繁忙的",
+definition: "(a.) busy",
+sentence: "The restaurant is fully booked on Christmas, thus it is expected to be hectic. ",
+options: ["hectic", "peddler", "exotic", "squeeze"]
+},
+{
+word: "sanitary",
+Chinese: "衛生的",
+definition: "(a.) clean and not dangerous for your health",
+sentence: " Cholera outbreaks are more common in places with poor sanitary conditions.",
+options: ["sanitary", "seasoning", "stove", "distil"]
+},
+{
+word: "distil",
+Chinese: "蒸餾",
+definition: "(v.) to extract the essential meaning or most important aspects by heating",
+sentence: "It's illegal to distil alcohol at home in some US states.",
+options: ["distil", "reimburse", "refund", "scoop"]
+},
+{
+word: "silverware",
+Chinese: "銀器(尤指餐具)",
+definition: "(n.) objects, especially knives, forks, spoons that made of silver ",
+sentence: "The restaurant use silverware made from France.",
+options: ["silverware", "peddler", "marinate", "heaty"]
+},
+{
+word: "peddler",
+Chinese: "小販",
+definition: "(n.) a person who travelled to different places to sell small goods",
+sentence: "Those peddlers often use low-level ingredients to keep down the cost.",
+options: ["peddler", "utensil", "roast", "intake"]
+},
+{
+word: "scoop",
+Chinese: "舀",
+definition: "(v.) to move something with a scoop",
+sentence: "He scooped three huge spoonfuls of ice cream into his bowl and covered them with chocolate sauce.",
+options: ["scoop", "stew", "grill", "chop"]
+},
+{
+word: "grill",
+Chinese: "炙烤(用烤架)",
+definition: "(v.) to cook food over fire or hot coals, usually on a metal frame",
+sentence: "Occasionally I might grill some meat and boil some vegetables to go with it.",
+options: ["grill", "rinse", "spoil", "scrub"]
+},
+{
+word: "roast",
+Chinese: "烘烤(用烤箱)",
+definition: "(v.) to cook food in an oven or over a fire",
+sentence: "Just roast the chicken in the oven and baste it in oil and lemon.",
+options: ["roast", "distil", "cater", "exotic"]
+},
+{
+word: "peel",
+Chinese: "剝",
+definition: "(v.) to remove the skin of something",
+sentence: "I showed my kids how to peel an apple with a knife.",
+options: ["peel", "roast", "sift", "appetite"]
+},
+{
+word: "rinse",
+Chinese: "沖洗",
+definition: "(v.) to use water to clean the soap or dirt from something",
+sentence: "Please rinse the broccoli carefully and then remove the stalks.",
+options: ["rinse", "squeeze", "cilantro", "mint"]
+},
+{
+word: "specialty",
+Chinese: "特產",
+definition: "(n.) a product that is extremely good in a place",
+sentence: "Oysters are a local specialty of the area.",
+options: ["specialty", "utensil", "supplier", "condiment"]
+},
+{
+word: "utensil",
+Chinese: "廚具",
+definition: "(n.) a tool with a particular use in a kitchen or house",
+sentence: "In the drawer was a selection of kitchen utensils - spoons, spatulas, knives, and whisks.",
+options: ["utensil", "entree", "cafeteria", "caterer"]
+},
+{
+word: "exotic",
+Chinese: "異國風情的",
+definition: "(a.) unusual because of coming from far away, especially a tropical country",
+sentence: " Visitors can taste many exotic cuisine in this restaurant.",
+options: ["exotic", "hearty", "beverage", "epicure"]
+},
+{
+word: "sift",
+Chinese: "過篩",
+definition: "(v.) to put flour or sugar through a sieve (= wire net shaped like a bowl) to break up large pieces",
+sentence: " When the cake is cooked, sift some icing sugar over the top of it.",
+options: ["sift", "adequate", "stew", "brew "]
+},
+{
+word: "stove",
+Chinese: "爐灶",
+definition: "(n.) a large device that is used to cook and heat food",
+sentence: "A portable gas stove is perfect for weekend camping trips.",
+options: ["stove", "silverware", "organ", "grill"]
+},
+{
+word: "brew",
+Chinese: "釀造；沖泡",
+definition: "(v.) to make beer or coffee",
+sentence: "You can save a lot of money if you brew your own beer at home.",
+options: ["brew", "gourmet", "refreshment", "roast "]
+},
+{
+word: "organic",
+Chinese: "有機的",
+definition: "(a.) not using artificial chemicals in the growing of plants",
+sentence: "My husband is quite picky and always wants me to buy organic fruits and vegetables.",
+options: ["organic", "leftover", "spoiled", "hectic"]
+},
+{
+word: "squeeze",
+Chinese: "擠壓",
+definition: "(v.) to press something firmly ,in order to change its shape, reduce its size, or remove liquid from it",
+sentence: "Cut the lemon in half and squeeze the juice into the bowl.",
+options: ["squeeze", "stove", "gourmet", "chop"]
+},
+{
+word: "marinate",
+Chinese: "醃漬",
+definition: "(v.) to coat or cover (food) with herbs, spices, etc. and let rest before cooking or serving",
+sentence: "The flavor of chicken is much better if you marinate it from the night before rather than the morning of the day you want to grill it.",
+options: ["marinate", "brew", "sift", "intake"]
+}
+
+]
