14f6a21bfac597dc501012bd2b1e954091ad2e4c
CammyLemon <cammy951003@gmail.com>
2025-05-14 15:49:23 +0800

    Create dining.js

---
+++ 題庫/dining.js
@@ -0,0 +1,219 @@
+vocabulary["dining"] = 
+[{
+word: "appetite",
+Chinese: "胃口",
+definition: "(n.) the feeling that you want to eat food",
+sentence: "In some cases, appetite loss may be a sign of an Anorexia.",
+options: ["appetite", "entree", "beverage", "mint"]
+},
+{
+word: "entree",
+Chinese: "主菜",
+definition: "(n.) the main dish of a meal",
+sentence: "Even at a nice restaurant, I still cut costs by ordering just an entree and little else.",
+options: ["entree", "serviette", "marinate", "silverware"]
+},
+{
+word: "serviette",
+Chinese: "餐巾",
+definition: "(n.) a napkin",
+sentence: "Take a serviette and wipe the rim, making sure there is no foil.",
+options: ["serviette", "scrub", "gourmet", "cilantro"]
+},
+{
+word: "appetizer",
+Chinese: "前菜",
+definition: "(n.) the first part of a meal",
+sentence: "We had spring rolls as an appetizer.",
+options: ["appetizer", "appetite", "cafeteria", "rinse"]
+},
+{
+word: "chop",
+Chinese: "劈；砍；剁碎，切碎",
+definition: "(v.) to cut something into pieces with an axe, knife, or other sharp instrument",
+sentence: "You've got to chop up the pork before stewing it.",
+options: ["chop", "scoop", "intake", "caterer"]
+},
+{
+word: "chop",
+Chinese: "帶骨豬排",
+definition: "(n.) a small piece of meat with a bone still in it",
+sentence: "This pork chop recipe with homemade mushroom gravy is my twist on a fancy restaurant meal we got on Valentine's Day.",
+options: ["chop", "compartment", "coriander", "dietitian"]
+},
+{
+word: "dietitian",
+Chinese: "營養師",
+definition: "(n.) a person who scientifically studies and gives advice about food and eating",
+sentence: "Check with your dietitian to be sure you’re eating enough protein, but not too much.",
+options: ["dietitian", "appetizer", "hearty", "utensil"]
+},
+{
+word: "beverage",
+Chinese: "飲料",
+definition: "(n.) a drink of any type",
+sentence: "Non-alcoholic beverages will also be provided at the opening banquet.",
+options: ["beverage", "mint", "peddler", "leftover"]
+},
+{
+word: "refreshment",
+Chinese: "茶點",
+definition: "(n.) something (such as food or drink) that refreshes",
+sentence: "After the workshop, light refreshments will be available in the conference room.",
+options: ["refreshment", "supplier", "cilantro", "stew"]
+},
+{
+word: "supplier",
+Chinese: "供應商",
+definition: "(n.) a company, person that provides things that people want or need, especially over a long period of time",
+sentence: "He said that he had got the drugs from his usual supplier.",
+options: ["supplier", "epicure", "hectic", "appetite"]
+},
+{
+word: "intake",
+Chinese: "攝取量",
+definition: "(n.) the amount of a particular substance that is eaten or drunk during a particular time",
+sentence: "It is good for your health to reduce your daily intake of salt.",
+options: ["intake", "fiber", "complimentary", "luncheon"]
+},
+{
+word: "adequate",
+Chinese: "充足的；能勝任的",
+definition: "(a.) enough or satisfactory for a particular purpose",
+sentence: "The meal you order for yourself is adequate for four people.",
+options: ["adequate", "hectic", "shift", "beverage"]
+},
+{
+word: "fiber",
+Chinese: "纖維",
+definition: "(n.) any of the thread-like parts that form plant or artificial material and can be made into cloth",
+sentence: "Apples are a good source of dietary fiber.",
+options: ["fiber", "layover", "hearty", "helping"]
+},
+{
+word: "complimentary",
+Chinese: "免費的",
+definition: "(a.) be given free",
+sentence: "There is a complimentary breakfast served from 7 a.m. to 10:30 a.m.",
+options: ["complimentary", "peddler", "condiment", "marinate"]
+},
+{
+word: "hearty",
+Chinese: "豐盛的；熱情的",
+definition: "(a.) (especially of food) in large amounts；being enthusiastic, energetic",
+sentence: "As we reached the restaurant, we received a hearty welcome from the waiters.",
+options: ["hearty", "seasoning", "stew", "entree"]
+},
+{
+word: "condiment",
+Chinese: "調味料",
+definition: "(n.) an ingredient or mixture of ingredients added to a dish while it's being cooked, aims to enhance or add to its flavor",
+sentence: "The woman is adding condiments to her meal .",
+options: ["condiment", "refreshment", "serviette", "leftover"]
+},
+{
+word: "seasoning",
+Chinese: "調味料",
+definition: "(n.) a substance added to food to improve its taste after the food is finished, such as ketchup",
+sentence: "The key to good cooking is to have the right seasoning",
+options: ["seasoning", "spoiling", "helping", "consisting"]
+},
+{
+word: "luncheon",
+Chinese: "午餐",
+definition: "(n.) lunch",
+sentence: "While eating luncheon meat may not be the healthiest meal option available, sometimes you need to throw together a quick sandwich.",
+options: ["luncheon", "utensil", "sanitary", "fiber"]
+},
+{
+word: "spoil",
+Chinese: "壞掉；變質",
+definition: "(v.) (of food) having become unfit for eating",
+sentence: " The dessert will spoil if you don't keep it in the fridge. ",
+options: ["spoil", "rinse", "chop", "organic"]
+},
+{
+word: "spoiled",
+Chinese: "被寵壞的",
+definition: "(a.) A child is allowed to do or have anything that they want, usually behave badly and do not show respect to other people.",
+sentence: "My sisters describe me as spoiled, though, because I was the youngest in the family.",
+options: ["spoiled", "peel", "stove", "grilled"]
+},
+{
+word: "gourmet",
+Chinese: "精緻美味的",
+definition: "(a.) (of food) very high quality",
+sentence: "The new gourmet food store at the end of the street has a great selection of French and Italian cheeses.",
+options: ["gourmet", "spoiled", "complimentary", "seasoning"]
+},
+{
+word: "gourmet",
+Chinese: "美食家；老饕",
+definition: "(n.) a person who knows a lot about food and cooking, and who enjoys eating high-quality food",
+sentence: "She is a gourmet who loves to try new and exotic foods.",
+options: ["gourmet", "silverware", "stove", "squeeze"]
+},
+{
+word: "epicure",
+Chinese: "老饕",
+definition: "(n.) a person who enjoys high quality food and drink",
+sentence: "This cookery book has been written by a real epicure.",
+options: ["epicure", "organic", "marinade", "luncheon"]
+},
+{
+word: "cilantro",
+Chinese: "芫荽；香菜葉",
+definition: "(n.) the leaves of the coriander plant, used to add flavour to food",
+sentence: "The soup is often topped with generous sprinkling of fresh chopped cilantro.",
+options: ["cilantro", "dietitian", "caterer", "gourmet"]
+},
+{
+word: "coriander",
+Chinese: "芫荽；香菜",
+definition: "(n.) a plant whose leaves or seeds are added to food to give a special flavour",
+sentence: "The sauces are made of several ingredients including chili, onion, red and green tomatoes, and coriander.",
+options: ["coriander", "specialty", "supplier", "peddler"]
+},
+{
+word: "mint",
+Chinese: "薄荷",
+definition: "(n.) a herb whose leaves have a strong, fresh smell and taste and are used for giving flavour to food",
+sentence: "I had a sore throat, so the doctor gave me medicine and a box of mints.",
+options: ["mint", "brew", "leftover", "cafeteria"]
+},
+{
+word: "mint",
+Chinese: "鑄幣廠",
+definition: "(n.) a place where the new coins are made",
+sentence: "You can visit the Royal Mint and see how coins are made.",
+options: ["mint", "caterer", "appetizer", "condiment"]
+},
+{
+word: "cafeteria",
+Chinese: "自助餐廳",
+definition: "(n.) where people collect food and drink from a serving area",
+sentence: "The hospital has a canteen restricted to staff and a cafeteria used by both staff and visitors.",
+options: ["cafeteria", "mint", "sanitary", "specialty"]
+},
+{
+word: "leftover",
+Chinese: "剩菜；殘餘",
+definition: "(n.) the part that has not been used or eaten when the other parts have been",
+sentence: "I'm going to heat up last night's leftovers for lunch.",
+options: ["leftover", "layover", "distill", "intake"]
+},
+{
+word: "caterer",
+Chinese: "宴席承辦者；外燴公司",
+definition: "(n.) a person or company that makes or serves food",
+sentence: "They prepare fresh vegetables for restaurants and caterers.",
+options: ["caterer", "silverware", "amenities", "coriander"]
+},
+{
+word: "helping",
+Chinese: "一份食物",
+definition: "(n.) an amount of food given to one person at one time ",
+sentence: "I'm trying to watch my calorie intake, so I only had a small helping of lasagna for dinner.",
+options: ["helping", "complimentary", "epicure", "scrub"]
+}
+]
